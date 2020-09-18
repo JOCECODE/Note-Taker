8c2d7747e50531a807f36bef4ea1af638b8185ee
JOCECODE <ulah57@gmail.com>
2020-09-18 00:04:14 -0700

    current workflow

--- Develop/routes/apiroutes.js
+++ Develop/routes/apiroutes.js
@@ -1,16 +1,28 @@
 const db = require("../db/db.json");
 const fs = require("fs");
 const path = require("path");
+const notesId = [];
 
 module.exports = (app) => {
     app.get("/api/notes", (req, res) => {
       return res.json(db);
     });
-    
+    app.get("/api/notes/:id", function(req, res){
+        var chosen = req.params.id;
+        console.log(chosen);
+        for (let i = 0; i < notesId.length; i++){
+            if (chosen === notesId[i]){
+                return res.json(notesId[i]);
+            }
+        }
+        return res.json(false)
+    })
+
     app.post("/api/notes", (req, res) => {
            const newNotes = req.body;
            db.push(newNotes);
-           fs.writeFile("../db/db.json", JSON.stringify(db), err => {
+           notesId.push(db);
+           fs.writeFile("../db/db.json", JSON.stringify(notesId), err => {
                if (err) throw err;
                console.log("wrote it");
                return res.json(db)
