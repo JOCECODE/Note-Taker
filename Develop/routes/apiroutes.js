84409e944a4a2328a80f2735f1664e62f89bb2b9
JOCECODE <ulah57@gmail.com>
2020-09-19 04:07:02 -0700

    added filter

--- Develop/routes/apiroutes.js
+++ Develop/routes/apiroutes.js
@@ -1,8 +1,7 @@
 const db = require("../db/db.json");
 const fs = require("fs");
-// const path = require("path");
-const notesId = [];
-let counter = 1
+let notesId = [];
+let counter = db.length;
 function addCounter(){
   counter = counter + 1;
 };
@@ -11,56 +10,30 @@ module.exports = (app) => {
     app.get("/api/notes", (req, res) => {
       return res.json(db);
     });
-    // app.get("/api/notes/:id", function(req, res){
-    //     var chosen = req.params.id;
-    //     console.log(chosen);
-    //     for (let i = 0; i < db.length; i++){
-    //         if (chosen === db[i]){
-    //             return res.json(db[i]);
-    //         }
-    //     }
-    //     return res.json(false)
-    // })
+    
 
     app.post("/api/notes", (req, res) => {
            const newNotes = req.body;
            addCounter(counter);
-           newNotes.id = counter;
+           newNotes.id = parseInt(counter);
            db.push(newNotes);
            fs.writeFile("../db/db.json", JSON.stringify(db), err => {
                if (err) throw err;
-               console.log("wrote it");
            });
            return res.json(db);
-        //    addCounter(counter);
-        //    console.log(newNotes)
-        //    let newId = counter;
-        //    console.log(newId);
-        //    newNotes.id = newId;
-        // //    counter = counter +1;
-        // //    console.log(counter);
-        // //    counter = JSON.stringify(counter);
-        // //    console.log(counter);
-        // //    newNotes.id = counter;
-        //    console.log(newNotes);
-        //     notesId.push(newNotes);
-        //     console.log(notesId)
-        //     console.log(db);
-        //    fs.writeFile("../db/db.json", JSON.stringify(notesId), err => {
-        //        if (err) throw err;
-        //        console.log("wrote it");
-        //        return res.json(db)
-        //    });
 })
-    app.delete("/api/notes/:id", (req,res) => {
-        const id = req.params.id;
-        const notes = req.body;
-        Book.removeBook(id, (err, notes)=>
-        {
-            if (err) {
-                throw err;
-            }
-            res.json(notes)
-        })
-    })    
+    app.delete("/api/notes/:id", (req,res) => {     
+    notesId = db; 
+    let chosen = req.params.id;
+    let less = notesId[chosen - 1].id;
+    console.log(notesId)
+     if(chosen === less.toString()){
+   let filteredNotes = notesId.filter(
+     (note) => note.id === parseInt(chosen));
+         fs.writeFile("../db/db.json", JSON.stringify(filteredNotes), (err) => {
+           if (err) throw err;
+         });
+     };
+        return res.json(db);
+});
 };
